682661c1f4ece1fe46de9af22bf33259505e9918
Guest <charlotte.davis.18@>
2020-02-10 21:37:50 -0500

    extractTokensFromFile

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -7,13 +7,27 @@
 
 //============================================================================
 //	TODO add necessary includes here
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+
 //============================================================================
 
 //============================================================================
 //	stuff you will need
 //============================================================================
 //TODO define a structure to track words and number of times they occur
+void extractTokensFromLine(std::string &myString) {
+	stringstream ss(myString);
+	string tempToken;
 
+	
+while (getline(ss, tempToken, CHAR_TO_SEARCH_FOR)) {
+	processToken(tempToken);
+	}
+}
 //TODO add a global array of entry structs (global to this file)
 
 //TODO add variable to keep track of next available slot in array
