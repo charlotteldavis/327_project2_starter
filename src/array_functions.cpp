d951b48bbea96235ad1c7cfd13e71a3645af64c0
Guest <charlotte.davis.18@>
2020-02-10 22:01:56 -0500

    functions from header added to cpp

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -23,16 +23,29 @@ void extractTokensFromLine(std::string &myString) {
 	stringstream ss(myString);
 	string tempToken;
 
-	
 while (getline(ss, tempToken, CHAR_TO_SEARCH_FOR)) {
 	processToken(tempToken);
 	}
 }
 //TODO add a global array of entry structs (global to this file)
-
+struct entry {
+	string word;
+	int number_occurences;
+};
 //TODO add variable to keep track of next available slot in array
 
 //TODO define all functions in header file
-
+void clearArray(myArray) {
+Array.Clear(myArray, 0, myArray.Length);
+}
+int getArraySize();
+std::string getArrayWordAt(int i);
+int getArrayWord_NumbOccur_At(int i);
+bool processFile(std::fstream &myfstream);
+void processLine(std::string &myString);
+void processToken(std::string &token);
+void closeFile(std::fstream& myfile);
+int writeArraytoFile(const std::string &outputfilename);
+void sortArray(constants::sortOrder so);
 //TODO look in utilities.h for useful functions, particularly strip_unwanted_chars!
 
